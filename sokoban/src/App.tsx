20b5d7759fda7369e81f8aa23141172d615565fc
Daniel Westergren <carl.dawe@gmail.com>
2024-03-19 16:08:29 +0100

    new

--- sokoban/src/App.tsx
+++ sokoban/src/App.tsx
@@ -42,27 +42,45 @@ function App() {
     83: { x: 0, y: 1 }, // S-key
     65: { x: -1, y: 0 }, // A-key
     68: { x: 1, y: 0 }, // D-key
-    
   };
 
-  
-
-  function handleKeyDown(e: any, d = keypress[e.keyCode], X = d.x, Y = d.y, staticObject = ["w", "b", "tb"], immutableObject = ["w"]) {
+  function handleKeyDown(
+    e: any,
+    d = keypress[e.keyCode],
+    X = d.x,
+    Y = d.y,
+    staticObject = ["w", "b", "tb"],
+    immutableObject = ["w"],
+    moveAbleObject = ["b"]
+  ) {
     let cMap = [...newGameBoard.map((row) => [...row])]; //  important so we don't mutate the state directly in global scope
     const cellOne = cMap[y + Y][x + X],
       cellTwo = cMap[y + Y * 2][x + X * 2]; // Calculate the indices of the cells affected by the movement cellOne => first cell, cellTwo => second cell
 
     if (immutableObject.includes(cellOne) || (staticObject.includes(cellOne) && staticObject.includes(cellTwo))) return; // Check if the movement is valid based on static objects array
 
-    if (cellOne.includes("b")){
+    if (cellOne.includes("b")) {
       cMap[y + Y][x + X] = cellOne.replace("b", "p");
       cMap[y + Y * 2][x + X * 2] += "b";
-    }
-    else{
+    } else {
       cMap[y + Y][x + X] += "p";
     }
     cMap[y][x] = cMap[y][x].replace("p", ""); // Update the game board based on the type of movement
-    
+
+    let unmoveableCells: { y: number; x: number; t: string }[] = [];
+    cMap.map((row, y: number) =>
+      row.map(
+        (cell, x: number) =>
+          (cell.includes(moveAbleObject) && 
+          (immutableObject.includes(row[x + 1]) || (immutableObject.includes(row[x - 1]))) && 
+          (immutableObject.includes(cMap[y + 1][x]) || immutableObject.includes(cMap[y - 1][x])) &&
+          unmoveableCells.push({ y: y, x: x, t: "BOX" }))
+      )
+    );
+    //see if any box is stuck
+
+    console.log(unmoveableCells);
+
     setNewGameBoard(cMap);
   }
 
